aafbcf64a8a39b97cc613be30bf4354b39097ceb
manojgetwealthy <manoj.baruah@wealthy.in>
2022-10-28 00:29:56 +0530

    add cleanup lifecycle

--- src/BaseSessionStore.ts
+++ src/BaseSessionStore.ts
@@ -3,8 +3,8 @@ import SessionStore from "./SessionStore";
 class BaseSessionStore<T> extends SessionStore<T> {
     isStoreReady: boolean;
     private _deps: Array<BaseSessionStore<any>>;
-    constructor(...deps: Array<BaseSessionStore<any>>) {
-        super();
+    constructor(name: string, ...deps: Array<BaseSessionStore<any>>) {
+        super(name);
         this.isStoreReady = false;
         this._deps = deps;
     }
@@ -16,6 +16,16 @@ class BaseSessionStore<T> extends SessionStore<T> {
                 await this._deps[i].init();
             }
         }
+    }
+
+    destroy() {
+        super.destroy();
+        if (this._deps) {
+            for (let i = 0; i < this._deps.length; i++) {
+                this._deps[i].destroy();
+            }
+        }
+        this.isStoreReady = false;
     }
 }
 
